ba124b65afd2e023518ec123582bd4d47a93f6ef
StrafelessPvP <Omegaopprison@gmail.com>
2018-01-12 16:27:15 -0800

    Add files via upload

--- src/me/remastered/java/Events/EventClass.java
+++ src/me/remastered/java/Events/EventClass.java
@@ -6,14 +6,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import me.remastered.java.Main;
+import me.remastered.java.Inventorys.CustomItemShop;
+import me.remastered.java.Items.CustomItems;
 import net.md_5.bungee.api.ChatColor;
 
 public class EventClass implements Listener {
 
+	private CustomItems ci2 = new CustomItems();
 	public String prefix = (ChatColor.GREEN + "HypedPvP>> ");
 	private Plugin plugin = Main.getPlugin(Main.class);
 
@@ -34,5 +39,33 @@ public class EventClass implements Listener {
 			}
 
 		}
+	}
+
+	@EventHandler
+	public void InvenClick(InventoryClickEvent event) {
+		Player player = (Player) event.getWhoClicked();
+
+		Inventory open = event.getClickedInventory();
+		ItemStack item = event.getCurrentItem();
+
+		if (open == null) {
+			return;
+		}
+		if (open.getName().equals(ChatColor.DARK_GREEN + "CustomItemShop")) {
+
+			event.setCancelled(true);
+
+			if (item == null || !item.hasItemMeta()) {
+				return;
+			}
+
+			if (item.getItemMeta().getDisplayName().equals(ChatColor.AQUA + "AXE OF THE GODS")) {
+				player.closeInventory();
+				CustomItemShop ci = new CustomItemShop();
+				ci2.giveItems(player);
+				ci.newInventory(player);
+			}
+		}
+
 	}
 }
--- src/me/remastered/java/Inventorys/CustomItemShop.java
+++ src/me/remastered/java/Inventorys/CustomItemShop.java
@@ -1,8 +1,14 @@
 package me.remastered.java.Inventorys;
 
+import java.util.ArrayList;
+
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 
 import me.remastered.java.Main;
@@ -15,7 +21,31 @@ public class CustomItemShop implements Listener {
 	public void newInventory(Player player){
 		Inventory i = plugin.getServer().createInventory(null,9, ChatColor.DARK_GREEN + "CustomItemShop");
 		player.openInventory(i);
-		//ToDo create the item
+		
+		ItemStack axe = new ItemStack(Material.DIAMOND_AXE, 1);
+		ItemMeta meta = axe.getItemMeta();
+		
+		ItemStack empty = new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte) 15);
+		ItemMeta emptyMeta = empty.getItemMeta();
+		emptyMeta.setDisplayName("'");
+		empty.setItemMeta(emptyMeta);
+		
+		meta.setDisplayName(ChatColor.AQUA + "AXE OF THE GODS");
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add(ChatColor.WHITE + "Used by THE GODS");
+		meta.setLore(lore);
+		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		axe.setItemMeta(meta);
+		
+		i.setItem(0,empty);
+		i.setItem(1,axe);
+		i.setItem(2,empty);
+		i.setItem(3,empty);
+		i.setItem(4,empty);
+		i.setItem(5,empty);
+		i.setItem(6,empty);
+		i.setItem(7,empty);
+		i.setItem(8,empty);;
 	}
 
 }
